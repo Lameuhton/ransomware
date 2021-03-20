339eaff8ce5baa7868eaf257ca30304337c9fdeb
unknown <randy.dock@live.be>
2021-03-20 19:43:24 +0100

    établissement des premieres fonctions

--- utile/message.py
+++ utile/message.py
@@ -1,5 +1,4 @@
 import json, pickle, base64
-
 def set_message(msg):
     """
     Cette fonction va transformer un message/dictionnaire en :
@@ -7,9 +6,13 @@ def set_message(msg):
     2.      Chiffrer en base64 (récupérer hash, key etc ?)
     3.      Transformer en Binaire pour être prêt à être envoyé via network
     """
+    # Conversion en JSON
+    converted_json = json.dumps(msg)
     # encodage du fichier via pickle
-    data_string = pickle.dumps(msg)
-    pass
+    convert_bytes = pickle.dumps(converted_json)
+    # encodage via base64
+    encoded = base64.b64encode(convert_bytes)
+    return encoded
 
 def get_message(data):
     """
@@ -18,8 +21,16 @@ def get_message(data):
     2.      déchiffrer via base 64
     3.      retransformer en dictionnaire python via JSON
     """
-    # Décode le message (qui est reçu en Bytes)
-    # Bytes -> String
-    message = pickle.loads(data)
-    pass
+    # Conversion d'encodé aux bytes
+    decoded = base64.b64decode(data)
+    # Conversion de bytes vers JSON
+    to_json = pickle.loads(decoded)
+    # conversion de json vers python
+    to_python = json.loads(to_json)
+    return to_python
 
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York"
+}
