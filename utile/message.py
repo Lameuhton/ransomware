e5d1ae26ba5ea8f555a4963af9261477817206e8
unknown <randy.dock@live.be>
2021-03-25 15:44:59 +0100

    message.py établit dans les grandes lignes

--- utile/message.py
+++ utile/message.py
@@ -1,36 +1,80 @@
-import json, pickle, base64
-def set_message(msg):
-    """
-    Cette fonction va transformer un message/dictionnaire en :
-    1.      Format JSON
-    2.      Chiffrer en base64 (récupérer hash, key etc ?)
-    3.      Transformer en Binaire pour être prêt à être envoyé via network
-    """
-    # Conversion en JSON
-    converted_json = json.dumps(msg)
-    # encodage du fichier via pickle
-    convert_bytes = pickle.dumps(converted_json)
-    # encodage via base64
-    encoded = base64.b64encode(convert_bytes)
-    return encoded
+# Constante
+DEBUG_MODE = False
 
-def get_message(data):
-    """
-    Cette fonction va transformer un message chiffré et en binaire en
-    1.      Retransformer en "str" via pickle
-    2.      déchiffrer via base 64
-    3.      retransformer en dictionnaire python via JSON
-    """
-    # Conversion d'encodé aux bytes
-    decoded = base64.b64decode(data)
-    # Conversion de bytes vers JSON
-    to_json = pickle.loads(decoded)
-    # conversion de json vers python
-    to_python = json.loads(to_json)
-    return to_python
+# tous les dictionnaires LIST_VICTIM
+LIST_VICTIM_REQ = { 'LIST_REQ': None }
+LIST_VICTIM_RESP = {'VICTIM','HASH','OS','DISKS','STATE','NB_FILES'}
+LIST_VICTIM_END = { 'LIST_END': None }
 
-x = {
-  "name": "John",
-  "age": 30,
-  "city": "New York"
+# tous les dictionnaire deHISTORY
+HISTORY_REQ = {'HIST_REQ'}
+HISTORY_RESP = {'HIST_RESP','TIMESTAMP','STATE','NB_FILES'}
+HISTORY_END = {'HIST_END'}
+
+# dictionnaire de CHANGE_STATE
+CHANGE_STATE = {'CHGSTATE','STATE','DECRYPT'}
+
+# liste de MESSAGE_TYPE
+MESSAGE_TYPE = {
+    'LIST_REQ': 'LIST_VICTIM_REQ',
+    'VICTIM': 'LIST_VICTIM_RESP',
+    'LIST_END' : 'LIST_VICTIM_END',
+    'HIST_REQ' : 'HISTORY_REQ',
+    'HIST_RESP' : 'HISTORY_RESP',
+    'HIST_END' : 'HISTORY_END',
+    'CHGSTATE' : 'CHANGE_STATE'
 }
+
+def set_message(select_msg, params=None):
+    if select_msg.upper() == 'LIST_VICTIMS_REQ':
+        return LIST_VICTIM_REQ
+
+    if select_msg.upper == 'LIST_VICTIM_RESP':
+        if len(params) != 6:
+            return None
+        LIST_VICTIM_RESP['VICTIM'] = params[0]
+        LIST_VICTIM_RESP['HASH'] = params[1]
+        LIST_VICTIM_RESP['OS'] = params[2]
+        LIST_VICTIM_RESP['DISKS'] = params[3]
+        LIST_VICTIM_RESP['STATE'] = params[4]
+        LIST_VICTIM_RESP['NB_FILES'] = params[5]
+        return LIST_VICTIM_RESP
+
+    if select_msg.upper() == 'LIST_VICTIM_END':
+        return LIST_VICTIM_END
+
+    if select_msg.upper == 'HISTORY_REQ':
+        if len(params) != 1:
+            return None
+        HISTORY_REQ['HIST_REQ'] = params[0]
+        return HISTORY_REQ
+
+    if select_msg.upper() == 'HISTORY_RESP':
+        if len(params) != 4:
+            return None
+        HISTORY_RESP['HISTO_RESP'] = params[0]
+        HISTORY_RESP['TIMESTAMP'] = params[1]
+        HISTORY_RESP['STATE'] = params[2]
+        HISTORY_RESP['NB_FILES'] = params[3]
+        return HISTORY_RESP
+
+    if select_msg.upper() == 'HISTORY_END':
+        if len(params) != 1:
+            return None
+        HISTORY_RESP['HIST_END'] = params[0]
+        return HISTORY_END
+
+    if select_msg.upper() == 'CHANGE_STATE':
+        if len(params) != 1:
+            return None
+        HISTORY_RESP['CHGSTATE'] = params[0]
+        return CHANGE_STATE
+
+
+def get_message_type(message):
+    first_key = list(message.keys())
+    #if DEBUG_MODE:
+    #    print("get_message_type()\n MESSAGE TYPE:", MESSAGE_TYPE[first_key])
+    return MESSAGE_TYPE[first_key]
+
+
