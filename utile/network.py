edeefd86bc438507dd501f2ba82de1891a679069
unknown <randy.dock@live.be>
2021-04-05 00:15:13 +0200

    network: Ajout try exept + logs error checklist, connect_to_serv retourne g et p si secured et True, ajout de commentaire. Security: ajout de commentaires, amélioration de DiffieHellman pour envoyer g et p ou le recevoir si g_and_p est coché

--- utile/network.py
+++ utile/network.py
@@ -1,49 +1,104 @@
 import socket
 import ipaddress
 import pickle
+from Cryptodome.Util.number import getPrime
+from random import randint
+
 HEADERSIZE = 10
 
-def start_net_serv_client(adresse="0.0.0.0",port=8380):
+
+def start_net_serv_client(adresse="localhost", port=8380):
+    """
+    Cette fonction crée un serveur socket écoutant pour une connexion
+    :param adresse: adresse sur laquelle le serveur se lie (de base localhost)
+    :param port: port sur lequelle il se lie (de base 8380)
+    :return: le socket du destinataire // est un nouvel objet socket utilisable pour envoyer et recevoir des
+            données sur la connexion
+    """
     # création d'un socket
     s = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)  # IPv4 & TCP
 
-    # Liaison de du socket sur l'ip 0.0.0.0 (local) et le port 8380
+    # Liaison de du socket sur l'ip localhost (local) et le port 8380
     s.bind((adresse, port))
-    
+
     # Permet au serveur d'attendre une connexion 
     s.listen(0)
 
-    # accepter une connexion
+    # accepter une connexion et reçoit le socket ainsi que l'adresse du destinataire
     conn, addr = s.accept()
-    # return:
-            # conn : est un nouvel objet socket utilisable pour envoyer et recevoir des données sur la connexion
-            # addr : adresse lié à l'autre socket de connexion; adresse du client
     print(f"Connected to {ipaddress.IPv4Address(addr[0])}:{addr[1]}")
     return conn
 
 
-def connect_to_serv(adresse="localhost", port=8380):
-    # Création du socket
-    socket_serv = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)  # IPv4 & TCP
-    # Se connecte au serveur d'ip "localhost" avec comme port 8380
-    socket_serv.connect((adresse, port))
-    return socket_serv
+def connect_to_serv(adresse="localhost", port=8380, secured=True):
+    """
+    Cette fonction crée un socket qui se connectera à un serveur
+    :param adresse: l'adresse du serveur sur lequelle se connecter (de base localhost)
+    :param port: port du serveur sur lequelle se connecter (de base 8380)
+    :param secured: Permet de générer les nombres g et p utile pour la méthode Diffie Hellman (valeur de base True)
+                    si appellé avec comme valeur None => net.connect_to_serv(secured=None) la génération sera ignorée
+    :return: Si secured = True :
+                                retourne son propre socket ainsi qu'un dictionnaire contenant g et p
+             Si secured = None :
+                                retourne son propre socket
+    """
+    try:
+        # Création du socket
+        socket_serv = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)  # IPv4 & TCP
+
+        # Se connecte au serveur d'ip "localhost" avec comme port 8380
+        socket_serv.connect((adresse, port))
+
+         # Génération d'un chiffre g et un nombre premier p
+        if secured:
+            dict_g_p = [randint(9, 99), getPrime(12)]
+            return socket_serv, dict_g_p
+        else:
+            return socket_serv
+
+    except socket.error as e:
+        print(f'[+] Impossible de se connecter :\n    {e}') # Retourne une erreur si la connexion ne fonctionne pas
+        return None, None
 
 
 def send_message(socket_serv, string_data, adresse="127.0.0.1", port=8380):
-    # envoie du message en local sur le port 8380
-    convert_bytes = pickle.dumps(string_data)
+    """
+    Cette fonction permet de convertir un message en binaire via le module pickle puis d'envoyer celui ci vers un autre
+    socket.
+    Une fois le message transformé, il va rajouter une entête de 10 octet permettant de connaitre la taille du message
+    message envoyé dans le format b'0000000026\x80\x04\x95\x0f\x00\x00\x00\x00\x00\x00\x00\x8c\x0bstring_data\x94.'
+    :param socket_serv: socket du client
+    :param string_data: données à envoyer (str, list, dictionnaire) à transformer en bytes
+    :param adresse: adresse sur laquelle envoyer le message
+    :param port: port sur lequelle envoyer le message
+    """
+    convert_bytes = pickle.dumps(string_data)  # Transforme le message en binaire
+
+    # Ajoute une entête de 10 octets renseignant sur la taille du message
     convert_bytes = bytes(f"{len(convert_bytes):010d}", 'utf-8') + convert_bytes
+
+    # Envoi du message
     socket_serv.sendto(convert_bytes, (adresse, port))
 
 
 def receive_message(socket):
-    # Attend de recevoir une donnée qui sera séparé en :
-        # data : les données
-        # addr: le duo addresse:port
+    """
+    Cette fonction permet de recevoir un message.
+    L'entête créée dans "send_message()" permet de limiter la charge réseau
+    La fonction va d'abord recevoir les 10 premiers caractères du message. Ces données indiqueront la taille du message.
+    Ensuite, la fonction décide de recevoir le reste du message qui à comme longueur pile poile le nombre de caractère
+    qu'il faut recevoir
+    :param socket: socket du client
+    :return: le message retourné en str, dictionnaire, liste, ...
+    """
 
+    # Reçoit en prioprité les 10 premiers octets étant l'entête renseignant sur la taille exacte du message
     header_from_message, addr = socket.recvfrom(HEADERSIZE)
-    data, addr = socket.recvfrom(int(header_from_message))
-    unpickled = pickle.loads(data)
-    return unpickled
 
+    # Reçoit le message avec la taille exacte du message pour limiter la charge réseau
+    data, addr = socket.recvfrom(int(header_from_message))
+
+    # Remet le message dans sa forme initiale (STR, liste, dictionnaire, ...)
+    unpickled = pickle.loads(data)
+
+    return unpickled
--- utile/security.py
+++ utile/security.py
@@ -2,10 +2,18 @@ from base64 import b64encode, b64decode
 import hashlib
 from Cryptodome.Cipher import AES
 from Cryptodome.Random import get_random_bytes
+from Cryptodome.Util.number import getRandomInteger
 import utile.network as net
-import secrets
+
 
 def AES_GCM_encrypt(plain_text, DiffieHellman_key):
+    """
+    Cette fonction chiffre les données via la méthode AES-256 GCM
+    :param plain_text: texte, dictionnaire, liste à chiffrer
+    :param DiffieHellman_key: mot de passe utilisé pour chiffrer la donnée.
+           (Se nomme DiffieHellman_key car ici le mot de passe utilsé sera un hash généré par cette fonction)
+    :return: un dictionnaire contenant le cipher texte, salt, nonce et tag
+    """
     # Generation du SALT
     salt = get_random_bytes(AES.block_size)
 
@@ -29,6 +37,16 @@ def AES_GCM_encrypt(plain_text, DiffieHellman_key):
 
 
 def AES_GCM_decrypt(enc_dict, DiffieHellman_key):
+    """
+    Cette fonction déchiffre les données via la méthode AES-256 GCM
+    :param enc_dict: Dictionnaire contenant le cipher texte, salt, nonce et tag
+            Si c'est généré par la fonction AES_GCM_encrypt de ce module pas de soucis.
+            Sinon il faut mettre les données sous cette forme :
+                {'cipher_text': 'xxxxx','salt': 'xxxxx','nonce': 'xxxxx','tag': 'xxxxx'}
+    :param DiffieHellman_key: Mot de passe utilisé pour déchiffrer la donnée.
+                      (Se nomme DiffieHellman_key car ici le mot de passe utilsé sera un hash généré par cette fonction)
+    :return: retourne la donné dans son état initial (STR, liste, dictionnaire, ...)
+    """
     # decode le dictionnaire avec base64 (retour vers la forme binaire)
     salt = b64decode(enc_dict['salt'])
     cipher_text = b64decode(enc_dict['cipher_text'])
@@ -47,17 +65,45 @@ def AES_GCM_decrypt(enc_dict, DiffieHellman_key):
 
     return decrypted
 
-def Diffie_Hellman_exchange_key(socket):
 
-    # Chiffre publiques (Peinture commune)
-    g = 9
-    p = 1001
+def Diffie_Hellman_exchange_key(socket, g_and_p=None):
+    """
+    Cette fonction permet de s'envoyer un mot de passe secret via un canal non sécurisé.
+    Grâce à de multiples calculs et envois sur le réseau, les données calculées auront le même résultat dans les deux
+    programmes mais pas à la vue d'une personne malveillante qui écouterait sur le réseau (wireshark?)
+
+    :param socket: socket du client
+    :param g_and_p:   liste contenant g et p
+                    - Valeur de base = None
+                        si None: s'attend à recevoir les données g et p. Si timeout de 10s arrêt de la fonction
+                        Si g_and_p est donné: envoi g et p au destinataire
+    :return: Si timeout: None
+            Sinon: le secret commun sous forme d'un hash 256
+    """
+    # Si  g et p ont été envoyés
+    if g_and_p:
+        # envoi g et p vers le destinataire
+        net.send_message(socket, g_and_p)
+        # décompose g et p pour les utiliser dans le code
+        g, p = g_and_p
+
+    # Si g et p n'ont pas été envoyés
+    else:
+        # tentative de recevoir le message
+        try:
+            socket.settimeout(10)
+            g, p = net.receive_message(socket)
+        # Si rien reçu au bout de 10 secondes : retourne un message d'erreur et renvoi None
+
+        except:
+            print(f'[+] Aucunes valeurs de g et p ont été trouvées au bout de 10 secondes\n')
+            return None
 
     # génération d'un chiffre aléatoire secret (peinture générée)
-    secret_number = secrets.randbits(12)
+    secret_number = getRandomInteger(12)
 
     # Création d'une clé (mélange de la couleur commune + couleur générée)
-    public_key = (g**secret_number) % p
+    public_key = (g ** secret_number) % p
 
     # Envoie de la clé vers le destinataire
     net.send_message(socket, public_key)
@@ -67,5 +113,6 @@ def Diffie_Hellman_exchange_key(socket):
 
     # Création du secret commun grâce aux clés reçues
     compute_key = (dest_public_key ** secret_number) % p
+
     common_secret = hashlib.sha256(str(compute_key).encode()).hexdigest()
     return common_secret
