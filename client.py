6457d4bd12c7bee213c22996b01ae4a529c518c2
twikson <80768394+twikson@users.noreply.github.com>
2021-04-24 21:26:18 +0200

    Delete client.py

--- client.py
+++
@@ -1,9 +0,0 @@
-import utile.network as net
-import utile.message as message
-
-socket, gp = net.connect_to_serv()
-net.send_message(socket, message.set_message('CHANGE_STATE', ['4', ['DECRYPT']]))
-
-while True:
-    data = net.receive_message(socket)
-    print(data)
