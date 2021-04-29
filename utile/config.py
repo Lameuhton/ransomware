1f3525db0ba5c4c5777283774f1eaf50a2dad49f
Lameuhton <s.tylaura@hotmail.com>
2021-04-29 18:41:27 +0200

    Commencement utile.config

--- utile/config.py
+++ utile/config.py
@@ -1,1 +1,31 @@
 import json
+import os
+import glob
+
+def lister_chemins_fichiers(disque):
+
+    disk = str(disque)
+    path_files = glob.glob(f'{disque}:/**/*', recursive = True)
+
+    return path_files
+
+def lire_fichier(path_file):
+
+    file = open(path_file, "r")
+    content = file.read()
+    file.close()
+
+    return content
+
+def creation_fichier_json():
+
+    setting = {'DISKS': [], # doit être une liste de string contenant les disques
+               'PATHS': [], # doit être une liste de string contenant les chemins vers les fichiers des disques
+               'FILE_EXT': [], # doit être une liste de string contenant les différentes extensions des fichiers des disques
+               'FREQ': 0, # doit être un int, est la fréquence d'émission en seconde
+               'KEY': '', # doit être une string contenant 512 caractères imprimables pour la clé de chiffrement
+               'STATE': '' # doit être une string contenant les états "INITIALIZE", "CRYPT", "PENDING", "DECRYPT" ou "PROTECTED"
+               }
+
+    with open("../configure/config/setting.json","w") as f:
+        json.dump(setting, f)
