5656de161fcf3179d7795cb1267ba16f11ba5363
twikson <80768394+twikson@users.noreply.github.com>
2021-05-04 12:42:28 +0200

    Delete client1.py

--- client1.py
+++
@@ -1,8 +0,0 @@
-import utile.network as net
-import utile.message as mess
-
-
-socket, gp = net.connect_to_serv()
-
-net.send_message(socket, mess.set_message('HISTORY_REQ', [4]))
-print(net.receive_message(socket), 'ehh')
